9dcfdbed58ef1d7de5e017545172f2954cb08f62
Your Name <your-email@example.com>
2025-11-19 22:32:34 +0200

    edit on thing

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,6 +80,7 @@ export const login = async (req, res) => {
       { expiresIn: "1d" }
     );
 
+    
     res.json({ message: "Login successful", token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
