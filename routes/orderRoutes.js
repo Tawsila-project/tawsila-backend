a3b7ca9dd3d63e28977abfc57d88a364a8369ddf
Your Name <your-email@example.com>
2025-11-26 14:19:45 +0200

    edit on order route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,12 +4,14 @@ import {
   // createOrder,
   getOrders,
   getOrder,
+  getPlacesStats,
+  getOrdersByRange,
   updateOrder,
   deleteOrder,
   // updateLocation,
+  getAvailableOrders,
   acceptOrder,
-  // getPendingOrdersForDriver,
-  // completeDelivery
+  updateDriverLocation 
 } from "../controllers/orderController.js";
 import { authMiddleware } from "../middleware/auth.js";
 
@@ -19,12 +21,12 @@ const router = Router();
 // router.use(authMiddleware(["staff", "admin"]));
 
 // router.post("/", createOrder);
+router.get('/orders/available', getAvailableOrders);
 router.get("/", getOrders);
+router.get("/places", authMiddleware(["admin"]), getPlacesStats);
+router.get("/logs-status", authMiddleware(["admin"]), getOrdersByRange);
 router.post("/accept", authMiddleware(["staff", "admin"]), acceptOrder);
-// router.post("/complete", completeDelivery);
-
-// router.get("/pending/:driverId" , getPendingOrdersForDriver);
-
+router.post('/location/update', updateDriverLocation);
 router.get("/:id", getOrder);
 router.put("/:id", authMiddleware(["staff", "admin"]), updateOrder);
 // router.post("/update-location", updateLocation); 
